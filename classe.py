12817302a3e58b091f943f5d381940a93e36a87c
bluemight <esnault.florent.pro@gmail.com>
2022-07-08 12:41:47 +0200

    Version 1.0.0
    - Création du repository.
    - Ajout de programme.py.
    - Ajout de classe.py.
    - Ajout de classe_test_unitaire.py.

---
+++ classe.py
@@ -0,0 +1,49 @@
+# .................................................................
+# NOTES : 
+# 
+
+# .................................................................
+# Imports librairie standard
+
+# Imports tiers
+
+# Imports locaux
+
+# .................................................................
+# Constantes
+
+
+# .................................................................
+# Classes
+class _MaClasse():
+    """TODO"""
+
+    # .................................................................
+    # Constructeur & méthodes spéciales
+
+    def __init__(self):
+        pass
+
+    # .................................................................
+    # Propriétés
+
+    @property
+    def ma_propriete(self):
+        pass # getter
+    
+    @ma_propriete.setter
+    def ma_propriete(self, valeur):
+        pass # setter
+
+    # .................................................................
+    # Méthodes
+
+    def ma_fonction(self, arg1, arg2):
+        pass
+
+
+
+
+
+
+
---
+++ classe_test_unitaire.py
@@ -0,0 +1,60 @@
+# .................................................................
+# NOTES : 
+# 
+
+# .................................................................
+# Imports librairie standard
+import unittest
+
+# Imports tiers
+
+# Imports locaux
+
+# .................................................................
+# Constantes
+
+
+# .................................................................
+# Classes
+class TestMaClasse(unittest.TestCase):
+    """Classe de test unitaire couvrant la classe XYZ."""
+
+    # .................................................................
+    # Préparations & clôture des tests
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def tearDown(self):
+        pass
+
+    # .................................................................
+    # Méthodes
+    
+    # .................................................................
+    # Tests
+    
+    def test_01_xxx(self):
+        pass
+
+    def test_02_xxx(self):
+        pass
+
+    def test_03_xxx(self):
+        pass
+
+
+
+
+
+
+
+
